4d4104571ccfd6cccf8050d3cca828602d71ebb1
Prashant mohite <113103569+PrashantMohite1@users.noreply.github.com>
2024-05-07 23:57:56 +0530

    Update setup-bridge-on-all-node.sh

--- setup-bridge-on-all-node.sh
+++ setup-bridge-on-all-node.sh
@@ -70,3 +70,29 @@ sudo systemctl enable containerd
 
 # ##### Apply sysctl params without reboot
 # sudo sysctl --system
+
+
+
+
+# Kubernetes Installation
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo kubeadm init
+
+
+
+
+
+
+
+
+
+
