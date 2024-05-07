ad31377c7cbb807f3b7a7b05da3fd4ad8efc1dae
Prashant mohite <113103569+PrashantMohite1@users.noreply.github.com>
2024-05-07 22:50:44 +0530

    Update t1.sh

--- t1.sh
+++ t1.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+# # Add Apt Repository for Kubernetes (all nodes)
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg
+# sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
 
-# Add Apt Repository for Kubernetes (all nodes)
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/kubernetes-xenial.gpg
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-
-
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
